6ddf0c89dc4357182029e72b8e8031133b428108
Pau Ramos <pauramosimo@gmail.com>
2024-10-06 23:47:54 +0200

    normi

--- minishell/src/env.c
+++ minishell/src/env.c
@@ -11,9 +11,6 @@
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-//sencillo buscamos que exista y tomamos el contenido desde despues del "=" y si no existe retornamos null
-//con respecto a ft_strchr_i la unica modificacion realizada es que esta devuelve la posicion entera en donde se encuentra
-
 
 char	*mini_getenv(char *var, char **envp, int n)
 {
@@ -28,47 +25,37 @@ char	*mini_getenv(char *var, char **envp, int n)
 		n2 = n;
 		if (n2 < ft_strchr_i(envp[i], '='))
 			n2 = ft_strchr_i(envp[i], '=');
-		//esta es la parte despues del "="
 		if (!ft_strncmp(envp[i], var, n2))
 			return (ft_substr(envp[i], n2 + 1, ft_strlen(envp[i])));
 		i++;
 	}
-	return NULL;
+	return (NULL);
 }
 
 char	**mini_setenv(char *var, char *value, char **envp, int n)
 {
-	int i[2];
-	char *aux[2];
+	int		i[2];
+	char	*aux[2];
 
 	if (n < 0)
 		n = ft_strlen(var);
-	//primer iterador en -1 para aplicar el auto aumento con ++i y te ahorras una linea
 	i[0] = -1;
-	//concatenamos en un array las variables de la env y su contenido (clave=valor) ejem: [aux0] ->PWD -> "[aux1] + contenido"
 	aux[0] = ft_strjoin(var, "=");
 	aux[1] = ft_strjoin(aux[0], value);
-	//liberamos el sobrante
 	free(aux[0]);
-	//recorremos las variables de entorno y comprobamos que en nuestro contenido no exista un "=" ademas de que no este vacio las env que puede pasar por el env -i
 	while (!ft_strchr(var, '=') && envp && envp[++i[0]])
 	{
-		//segundo iterador igual al numero de letras de la clave
 		i[1] = n;
-		//se va iterando y cambiado el iterador dependiendo de en que variable de entorno nos encontremos, en caso de encontrar esta se hace una actualizacion
-		//esto para casos en donde existan cambios en el shlv o la ruta del pwd
 		if (i[1] < ft_strchr_i(envp[i[0]], '='))
 			i[1] = ft_strchr_i(envp[i[0]], '=');
-		//se comprueba si la variable de entorno en donde nos encontramos como pwd, el iterador i[1] = 3 y se comprobaran estas 3 posiciones y cambiara
-		//dependiendo de en que iteracion estamos este valor cambiara como con oldpwd o home, etc.
 		if (!ft_strncmp(envp[i[0]], var, i[1]))
-		{	aux[0] = envp[i[0]];
+		{
+			aux[0] = envp[i[0]];
 			envp[i[0]] = aux[1];
 			free(aux[0]);
-			return (envp);	
+			return (envp);
 		}
 	}
-	//en caso de no encontrarse en las envp se añade esta nueva en la ultima posicion
 	envp = ft_extend_matrix(envp, aux[1]);
 	free(aux[1]);
 	return (envp);
@@ -76,27 +63,29 @@ char	**mini_setenv(char *var, char *value, char **envp, int n)
 
 static	int		where_envp(char *argc, char **envp)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(envp[i])
+	while (envp[i])
 	{
 		if (ft_strchr(envp[i], '='))
 		{
-			if (!ft_strncmp(envp[i], argc, ft_strlen(envp[i]) - ft_strlen(ft_strchr(envp[i], '='))))
+			if (!ft_strncmp(envp[i], argc, ft_strlen(envp[i]) -
+					ft_strlen(ft_strchr(envp[i], '='))))
 				return (i);
 		}
-		else{
-			if (!ft_strncmp(envp[i], argc, ft_strlen(envp[i]) - ft_strlen(ft_strchr(envp[i], '\0'))))
+		else
+		{
+			if (!ft_strncmp(envp[i], argc, ft_strlen(envp[i]) -
+					ft_strlen(ft_strchr(envp[i], '\0'))))
 				return (i);
 		}
-		
 		i++;
 	}
 	return (0);
 }
 
-static	int 	var_in_envp(char *argc, char **envp, int ij[2])
+static	int		var_in_envp(char *argc, char **envp, int ij[2])
 {
 	ij[1] = 0;
 
@@ -108,11 +97,7 @@ static	int 	var_in_envp(char *argc, char **envp, int ij[2])
 	}
 	return (0);
 }
-//revizar el export
-#include <stdlib.h>
-#include <string.h>
 
-// Función que concatena dos cadenas y libera la primera
 char	*ft_strjoin_free(char *s1, const char *s2)
 {
 	char	*result;
@@ -131,88 +116,69 @@ char	*ft_strjoin_free(char *s1, const char *s2)
 	}
 	strcpy(result, s1);
 	strcat(result, s2);
-	free(s1);  // Liberamos la primera cadena
+	free(s1);
 	return (result);
 }
 
 void	show_export_env(char *str)
 {
-	int i;
-	int lock;
-	
+	int	i;
+	int	lock;
+
 	i = 0;
 	lock = 0;
 	while (str[i])
 	{
-		write(1,&str[i],1);
+		write(1, &str[i], 1);
 		if (str[i] == '=' && lock == 0)
 		{
-			write(1,"\"",1);
+			write(1, "\"", 1);
 			lock++;
 		}
 		i++;
 	}
 	if (lock != 0)
-		write(1,"\"",1);
-	write(1,"\n",1);
+		write(1, "\"", 1);
+	write(1, "\n", 1);
 }
 
-char **ft_comprobar_prompt(t_prompt *prompt)
+char	**ft_comprobar_prompt(t_prompt *prompt)
 {
-	t_list *puntero;
-	int i;
+	t_list	*puntero;
+	int		i;
 
 	puntero = (t_list *)prompt->cmds;
 	while (puntero->next)
 	{
-		if (!ft_strncmp(((t_mini*)puntero->content)->full_cmd[0],"export",6))
-		{
-			//printf("entro %s\n",((t_mini*)puntero->content)->full_cmd[0]);
+		if (!ft_strncmp(((t_mini*)puntero->content)->full_cmd[0], "export", 6))
 			return ((t_mini*)puntero->content)->full_cmd;
-		}
-		//printf("no entro %s\n",((t_mini*)puntero->content)->full_cmd[0]);
 		puntero = puntero->next;
 	}
-	if (!ft_strncmp(((t_mini*)puntero->content)->full_cmd[0],"export",6))
-	{
-			//printf("entro %s\n",((t_mini*)puntero->content)->full_cmd[0]);
+	if (!ft_strncmp(((t_mini*)puntero->content)->full_cmd[0], "export", 6))
 		return ((t_mini*)puntero->content)->full_cmd;
-	}
-	return NULL;
+	return (NULL);
 
 }
 
 int	mini_export(t_prompt *prompt)
 {
-	int	ij[2];
-	int	pos;
+	int		ij[2];
+	int		pos;
 	char	**argc;
 	char	*key;
 	char	*value;
 	char	*new_var;
 
-	//argc = ((t_mini *)prompt->cmds->content)->full_cmd;
 	argc = ft_comprobar_prompt(prompt);
-	//ft_comprobar_prompt(prompt);
-	/**int t = 0;
-	while(argc[t])
-	{
-		printf("dentro %s\n", argc[t]);
-		t++;
-	}*/
-	if(!export_wrong(argc))
-	{
+	if (!export_wrong(argc))
 		return(1);
-	}
-	// Si no hay argumentos, lista todas las variables de entorno
 	if (ft_matrixlen(argc) == 1)
 	{
-
-		char **env = prompt->envp;
+		char  **env = prompt->envp;
 		while (*env)
 		{
 			ft_putstr_fd("declare -x ", 1);
-			show_export_env(*env); // Imprime cada variable de entorno
+			show_export_env(*env);
 			env++;
 		}
 		return (0);
@@ -225,24 +191,17 @@ int	mini_export(t_prompt *prompt)
 			char *equal_sign = ft_strchr(argc[ij[0]], '=');
 			if (equal_sign)
 			{
-				// Separar la clave y el valor
 				key = ft_substr(argc[ij[0]], 0, equal_sign - argc[ij[0]]);
 				value = ft_strdup(equal_sign + 1);
 			}
 			else
 			{
-				// Solo clave, sin valor
 				key = ft_strdup(argc[ij[0]]);
-				//value = ft_strdup("");
 				value = NULL;
-				// printf("%s",value); aquí el nulo que buscaba
 			}
-
-			// Buscar la variable en el entorno
 			pos = var_in_envp(key, prompt->envp, ij);
 			if (pos == 1)
 			{
-				// Reemplazar el valor existente
 				pos = where_envp(key, prompt->envp);
 				free(prompt->envp[pos]);
 				if (!value)
@@ -257,7 +216,6 @@ int	mini_export(t_prompt *prompt)
 			}
 			else if (!pos)
 			{
-				// Añadir la nueva variable
 				if (!value)
 				{
 					new_var = ft_strdup(key);
@@ -281,27 +239,23 @@ int	mini_export(t_prompt *prompt)
 
 int	export_wrong(char **argc)
 {
-	//revisar con pipe
-	// if (ft_strncmp(argc[x], "export", 6))
-	// 	x++;
 	if (ft_matrixlen(argc) >= 2 && argc[1][0] == '-')
 	{
 		mini_perror(argc, INV_OPTION, NULL,  1);
-		return 0;	
+		return (0);	
 	}
 	else if (!if_alfnum_(argc))
 	{
 		mini_perror(argc, NOT_VAL_IDENT, NULL, 1);
-		return 0;	
+		return (0);	
 	}
 	return (1);
-	//falta free
 }
 
-int if_alfnum_(char **cmds)
+int	if_alfnum_(char **cmds)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = -1;
 	while (cmds[++i])
@@ -317,35 +271,8 @@ int if_alfnum_(char **cmds)
 				return (1);
 		}
 	}
-	return(1);
+	return (1);
 }
-/* 
-int	mini_export(t_prompt *prompt)
-{
-	int		ij[2];
-	int		pos;
-	char	**argv;
-
-	argv = ((t_mini *)prompt->cmds->content)->full_cmd;
-	if (ft_matrixlen(argv) >= 2)
-	{
-		ij[0] = 1;
-		while (argv[ij[0]])
-		{
-			pos = var_in_envp(argv[ij[0]], prompt->envp, ij);
-			if (pos == 1)
-			{
-				free(prompt->envp[ij[1]]);
-				prompt->envp[ij[1]] = ft_strdup(argv[ij[0]]);
-			}
-			else if (!pos)
-				prompt->envp = ft_extend_matrix(prompt->envp, argv[ij[0]]);
-			ij[0]++;
-		}
-	}
-	return (0);
-}
-*/
 
 int	mini_unset(t_prompt *prompt)
 {
